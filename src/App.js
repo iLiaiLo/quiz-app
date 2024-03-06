a423e1c3db5409fea9f48b85f832e7165e6262e6
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-06 23:06:57 +0400

    added additional info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.js";
+
 
 function App() {
+
+  const [increment,SetIncrement]=React.useState(0);
+  const [score,setScore]=React.useState(0);
+  const [clicked,setclicked]=React.useState(false);
+  const [renderContent,setRendercontent]=React.useState(true);
+  const [start,setStart]=React.useState(true)
+
+  function Inc(){
+    SetIncrement(prew=>prew+1);
+    setclicked(false);
+  }
+
+  function checkAnswer(item){
+     setclicked(true);
+     
+     if(item.isCorrect){
+      setScore(prev=>prev+1)
+      if(item.id===1||item.id===2||item.id===3||item.id===4){
+        item.style="lightgreen";
+      }
+   
+     }
+     else{
+      if(item.id===1||item.id===2||item.id===3||item.id===4){
+        item.style="red";
+      }
+     }
+  }
+  
+  function displayContent(){
+    setRendercontent(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {start && <div className="StartContent">
+        <button className="startButton" onClick={()=>setStart(false)}>Start quiz</button>
+        </div>
+      }
+
+      {renderContent && start===false ? (<div className="questions">
+        <h1 className="question">{data[increment].question}</h1>
+        <div className="choices">
+          {data[increment].answerOptions.map((answer)=>{
+            if(answer.isCorrect && clicked){
+              return(
+                <button style={{background:"lightgreen"}} onClick={()=>{checkAnswer(answer)}} disabled={clicked}>{answer.ans}</button>
+                )
+            }
+              return(
+            <button style={{background:answer.style}} onClick={()=>{checkAnswer(answer)}} disabled={clicked}>{answer.ans}</button>
+            )
+          })}
+      
+        </div>
+
+      </div>
+      ):(<div className="finishcontent" style={{display:start===false?"flex":"none"}}>
+        <h1>Your score is {score} out of {data.length}</h1>
+        </div>)}
+    <div className="nextGetresButton">
+      {renderContent && clicked && increment<data.length-1 && <button onClick={Inc}>Next</button>}
+      {renderContent && clicked && increment===data.length-1 && <button onClick={displayContent}>Show result</button>}
     </div>
+  
+      
+    </div>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data=[
+    {
+      question:"What planet is the biggest in the solar system?",
+      answerOptions:[
+      {ans:"Earth",isCorrect:false,id:1,style:""},
+      {ans:"Saturn",isCorrect:false,id:2,style:""},
+      {ans:"Jupiter",isCorrect:true,id:3,style:""},
+      {ans:"Neptune",isCorrect:false,id:4,style:""}
+    ]
+    },
+    {
+      question:"What country is mount Fuji located in?",
+      answerOptions:[
+      {ans:"Japan",isCorrect:true,id:1,style:""},
+      {ans:"Germany",isCorrect:false,id:2,style:""},
+      {ans:"China",isCorrect:false,id:3,style:""},
+      {ans:"USA",isCorrect:false,id:4,style:""}
+  
+    ]
+    },
+    {
+      question:"What position is earth in solar sistem?",
+      answerOptions:[
+      {ans:"second",isCorrect:false,id:1,style:""},
+      {ans:"third",isCorrect:true,id:2,style:""},
+      {ans:"fourth",isCorrect:false,id:3,style:""},
+      {ans:"fifth",isCorrect:false,id:4,style:""}
+    ]
+    },
+    {
+      question:"What is the longest river on the earth?",
+      answerOptions:[
+      {ans:"Danube",isCorrect:false,id:1,style:""},
+      {ans:"Yangtze",isCorrect:false,id:2,style:""},
+      {ans:"Nile",isCorrect:true,id:3,style:""},
+      {ans:"Amazon",isCorrect:false,id:4,style:""}
+    ]
+    },
+    { 
+      question:"What is the tallest mauntain on the earth?",
+      answerOptions:[
+      {ans:"Elbrus",isCorrect:false,id:1,style:""},
+      {ans:"Everest",isCorrect:true,id:2,style:""},
+      {ans:"Kilimanjaro",isCorrect:false,id:3,style:""},
+      {ans:"Aconcagua",isCorrect:false,id:4,style:""}
+    ]
+    },
+    {
+      question:"What country is Machu Picchu located in?",
+      answerOptions:[
+      {ans:"Brazil",isCorrect:false,id:1,style:""},
+      {ans:"Kenya",isCorrect:false,id:2,style:""},
+      {ans:"Namibia",isCorrect:false,id:3,style:""},
+      {ans:"Peru",isCorrect:true,id:4,style:""}
+      ]
+    },
+    {
+      question:"How many contitents are on the earth?",
+      answerOptions:[
+      {ans:"7",isCorrect:true,id:1,style:""},
+      {ans:"6",isCorrect:false,id:2,style:""},
+      {ans:"5",isCorrect:false,id:3,style:""},
+      {ans:"8",isCorrect:false,id:4,style:""}
+      ]
+    },
+    {
+      question:"What is the biggest iceland on earth?",
+      answerOptions:[
+      {ans:"Greenland",isCorrect:true,id:1,style:""},
+      {ans:"Madagascar",isCorrect:false,id:2,style:""},
+      {ans:"Borneo",isCorrect:false,id:3,style:""},
+      {ans:"Sumatra",isCorrect:false,id:4,style:""}
+      ]
+    },
+    {
+      question:"what is the biggest ocean on earth?",
+      answerOptions:[
+      {ans:"Atlantic",isCorrect:false,id:1,style:""},
+      {ans:"Pacific",isCorrect:true,id:2,style:""},
+      {ans:"Indian",isCorrect:false,id:3,style:""},
+      {ans:"Arctic",isCorrect:false,id:4,style:""}
+      ]
+    },
+    {
+      question:"What planet is tallest mountain located in in the solar sistem?",
+      answerOptions:[
+      {ans:"Uranus",isCorrect:false,id:1,style:""},
+      {ans:"Saturn",isCorrect:false,id:2,style:""},
+      {ans:"Mars",isCorrect:true,id:3,style:""},
+      {ans:"Earth",isCorrect:false,id:4,style:""}
+      ]
+    }
+  ]
+  export default data;
+  
